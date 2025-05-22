7aa4a8b756a3476a06ad02af411f977df4e6e10f
augustosensei <augustosensei@gmail.com>
2025-05-21 23:40:45 -0300

    Formato

--- microservice1/build/reports/tests/test/js/report.js
+++ microservice1/build/reports/tests/test/js/report.js
@@ -1,194 +1,194 @@
-(function (window, document) {
-    "use strict";
+(function(window, document) {
+	"use strict";
 
-    var tabs = {};
+	var tabs = {};
 
-    function changeElementClass(element, classValue) {
-        if (element.getAttribute("className")) {
-            element.setAttribute("className", classValue);
-        } else {
-            element.setAttribute("class", classValue);
-        }
-    }
+	function changeElementClass(element, classValue) {
+		if (element.getAttribute("className")) {
+			element.setAttribute("className", classValue);
+		} else {
+			element.setAttribute("class", classValue);
+		}
+	}
 
-    function getClassAttribute(element) {
-        if (element.getAttribute("className")) {
-            return element.getAttribute("className");
-        } else {
-            return element.getAttribute("class");
-        }
-    }
+	function getClassAttribute(element) {
+		if (element.getAttribute("className")) {
+			return element.getAttribute("className");
+		} else {
+			return element.getAttribute("class");
+		}
+	}
 
-    function addClass(element, classValue) {
-        changeElementClass(element, getClassAttribute(element) + " " + classValue);
-    }
+	function addClass(element, classValue) {
+		changeElementClass(element, getClassAttribute(element) + " " + classValue);
+	}
 
-    function removeClass(element, classValue) {
-        changeElementClass(element, getClassAttribute(element).replace(classValue, ""));
-    }
+	function removeClass(element, classValue) {
+		changeElementClass(element, getClassAttribute(element).replace(classValue, ""));
+	}
 
-    function initTabs() {
-        var container = document.getElementById("tabs");
+	function initTabs() {
+		var container = document.getElementById("tabs");
 
-        tabs.tabs = findTabs(container);
-        tabs.titles = findTitles(tabs.tabs);
-        tabs.headers = findHeaders(container);
-        tabs.select = select;
-        tabs.deselectAll = deselectAll;
-        tabs.select(0);
+		tabs.tabs = findTabs(container);
+		tabs.titles = findTitles(tabs.tabs);
+		tabs.headers = findHeaders(container);
+		tabs.select = select;
+		tabs.deselectAll = deselectAll;
+		tabs.select(0);
 
-        return true;
-    }
+		return true;
+	}
 
-    function getCheckBox() {
-        return document.getElementById("line-wrapping-toggle");
-    }
+	function getCheckBox() {
+		return document.getElementById("line-wrapping-toggle");
+	}
 
-    function getLabelForCheckBox() {
-        return document.getElementById("label-for-line-wrapping-toggle");
-    }
+	function getLabelForCheckBox() {
+		return document.getElementById("label-for-line-wrapping-toggle");
+	}
 
-    function findCodeBlocks() {
-        var spans = document.getElementById("tabs").getElementsByTagName("span");
-        var codeBlocks = [];
-        for (var i = 0; i < spans.length; ++i) {
-            if (spans[i].className.indexOf("code") >= 0) {
-                codeBlocks.push(spans[i]);
-            }
-        }
-        return codeBlocks;
-    }
+	function findCodeBlocks() {
+		var spans = document.getElementById("tabs").getElementsByTagName("span");
+		var codeBlocks = [];
+		for (var i = 0; i < spans.length; ++i) {
+			if (spans[i].className.indexOf("code") >= 0) {
+				codeBlocks.push(spans[i]);
+			}
+		}
+		return codeBlocks;
+	}
 
-    function forAllCodeBlocks(operation) {
-        var codeBlocks = findCodeBlocks();
+	function forAllCodeBlocks(operation) {
+		var codeBlocks = findCodeBlocks();
 
-        for (var i = 0; i < codeBlocks.length; ++i) {
-            operation(codeBlocks[i], "wrapped");
-        }
-    }
+		for (var i = 0; i < codeBlocks.length; ++i) {
+			operation(codeBlocks[i], "wrapped");
+		}
+	}
 
-    function toggleLineWrapping() {
-        var checkBox = getCheckBox();
+	function toggleLineWrapping() {
+		var checkBox = getCheckBox();
 
-        if (checkBox.checked) {
-            forAllCodeBlocks(addClass);
-        } else {
-            forAllCodeBlocks(removeClass);
-        }
-    }
+		if (checkBox.checked) {
+			forAllCodeBlocks(addClass);
+		} else {
+			forAllCodeBlocks(removeClass);
+		}
+	}
 
-    function initControls() {
-        if (findCodeBlocks().length > 0) {
-            var checkBox = getCheckBox();
-            var label = getLabelForCheckBox();
+	function initControls() {
+		if (findCodeBlocks().length > 0) {
+			var checkBox = getCheckBox();
+			var label = getLabelForCheckBox();
 
-            checkBox.onclick = toggleLineWrapping;
-            checkBox.checked = false;
+			checkBox.onclick = toggleLineWrapping;
+			checkBox.checked = false;
 
-            removeClass(label, "hidden");
-         }
-    }
+			removeClass(label, "hidden");
+		}
+	}
 
-    function switchTab() {
-        var id = this.id.substr(1);
+	function switchTab() {
+		var id = this.id.substr(1);
 
-        for (var i = 0; i < tabs.tabs.length; i++) {
-            if (tabs.tabs[i].id === id) {
-                tabs.select(i);
-                break;
-            }
-        }
+		for (var i = 0; i < tabs.tabs.length; i++) {
+			if (tabs.tabs[i].id === id) {
+				tabs.select(i);
+				break;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    function select(i) {
-        this.deselectAll();
+	function select(i) {
+		this.deselectAll();
 
-        changeElementClass(this.tabs[i], "tab selected");
-        changeElementClass(this.headers[i], "selected");
+		changeElementClass(this.tabs[i], "tab selected");
+		changeElementClass(this.headers[i], "selected");
 
-        while (this.headers[i].firstChild) {
-            this.headers[i].removeChild(this.headers[i].firstChild);
-        }
+		while (this.headers[i].firstChild) {
+			this.headers[i].removeChild(this.headers[i].firstChild);
+		}
 
-        var h2 = document.createElement("H2");
+		var h2 = document.createElement("H2");
 
-        h2.appendChild(document.createTextNode(this.titles[i]));
-        this.headers[i].appendChild(h2);
-    }
+		h2.appendChild(document.createTextNode(this.titles[i]));
+		this.headers[i].appendChild(h2);
+	}
 
-    function deselectAll() {
-        for (var i = 0; i < this.tabs.length; i++) {
-            changeElementClass(this.tabs[i], "tab deselected");
-            changeElementClass(this.headers[i], "deselected");
+	function deselectAll() {
+		for (var i = 0; i < this.tabs.length; i++) {
+			changeElementClass(this.tabs[i], "tab deselected");
+			changeElementClass(this.headers[i], "deselected");
 
-            while (this.headers[i].firstChild) {
-                this.headers[i].removeChild(this.headers[i].firstChild);
-            }
+			while (this.headers[i].firstChild) {
+				this.headers[i].removeChild(this.headers[i].firstChild);
+			}
 
-            var a = document.createElement("A");
+			var a = document.createElement("A");
 
-            a.setAttribute("id", "ltab" + i);
-            a.setAttribute("href", "#tab" + i);
-            a.onclick = switchTab;
-            a.appendChild(document.createTextNode(this.titles[i]));
+			a.setAttribute("id", "ltab" + i);
+			a.setAttribute("href", "#tab" + i);
+			a.onclick = switchTab;
+			a.appendChild(document.createTextNode(this.titles[i]));
 
-            this.headers[i].appendChild(a);
-        }
-    }
+			this.headers[i].appendChild(a);
+		}
+	}
 
-    function findTabs(container) {
-        return findChildElements(container, "DIV", "tab");
-    }
+	function findTabs(container) {
+		return findChildElements(container, "DIV", "tab");
+	}
 
-    function findHeaders(container) {
-        var owner = findChildElements(container, "UL", "tabLinks");
-        return findChildElements(owner[0], "LI", null);
-    }
+	function findHeaders(container) {
+		var owner = findChildElements(container, "UL", "tabLinks");
+		return findChildElements(owner[0], "LI", null);
+	}
 
-    function findTitles(tabs) {
-        var titles = [];
+	function findTitles(tabs) {
+		var titles = [];
 
-        for (var i = 0; i < tabs.length; i++) {
-            var tab = tabs[i];
-            var header = findChildElements(tab, "H2", null)[0];
+		for (var i = 0; i < tabs.length; i++) {
+			var tab = tabs[i];
+			var header = findChildElements(tab, "H2", null)[0];
 
-            header.parentNode.removeChild(header);
+			header.parentNode.removeChild(header);
 
-            if (header.innerText) {
-                titles.push(header.innerText);
-            } else {
-                titles.push(header.textContent);
-            }
-        }
+			if (header.innerText) {
+				titles.push(header.innerText);
+			} else {
+				titles.push(header.textContent);
+			}
+		}
 
-        return titles;
-    }
+		return titles;
+	}
 
-    function findChildElements(container, name, targetClass) {
-        var elements = [];
-        var children = container.childNodes;
+	function findChildElements(container, name, targetClass) {
+		var elements = [];
+		var children = container.childNodes;
 
-        for (var i = 0; i < children.length; i++) {
-            var child = children.item(i);
+		for (var i = 0; i < children.length; i++) {
+			var child = children.item(i);
 
-            if (child.nodeType === 1 && child.nodeName === name) {
-                if (targetClass && child.className.indexOf(targetClass) < 0) {
-                    continue;
-                }
+			if (child.nodeType === 1 && child.nodeName === name) {
+				if (targetClass && child.className.indexOf(targetClass) < 0) {
+					continue;
+				}
 
-                elements.push(child);
-            }
-        }
+				elements.push(child);
+			}
+		}
 
-        return elements;
-    }
+		return elements;
+	}
 
-    // Entry point.
+	// Entry point.
 
-    window.onload = function() {
-        initTabs();
-        initControls();
-    };
-} (window, window.document));
+	window.onload = function() {
+		initTabs();
+		initControls();
+	};
+}(window, window.document));
